2a503e611278b4ac8efab4838f6b8e8cd2ae8c90
Damikaz205 <damikaz205@student.polsl.pl>
2019-10-02 13:33:18 +0200

    zmieniono plik main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Umiesc to na GitHubie");
+        System.out.println("Hello - Umiesc to na GitHubie");
  }
 }
